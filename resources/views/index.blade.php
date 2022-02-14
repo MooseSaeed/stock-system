00fe79523e9a2964fd942bb777f51cab37012b07
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-02-14 13:23:40 +0200

    Modifying View

---
+++ resources/views/index.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Tailwind -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
+</head>
+
+<body class="antialiased">
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        @if (Route::has('login'))
+        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            @auth
+            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+            @else
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+            @if (Route::has('register'))
+            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+            @endif
+            @endauth
+        </div>
+        @endif
+
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                <div class="grid grid-cols-1 md:grid-cols-2">
+
+                    <div class="p-6 border-t border-gray-200 dark:border-gray-700">
+                        <div class="flex items-center">
+                            <div class="ml-4 text-lg leading-7 font-semibold">
+                                <a href="https://laravel-news.com/" class="underline text-gray-900 dark:text-white">
+                                    FrontEnd
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="ml-12">
+                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum minus beatae fuga
+                                maxime totam voluptatibus fugit impedit tempora, odio quos? Placeat magnam voluptas
+                                veritatis nesciunt soluta, reprehenderit voluptatibus unde consequatur quam nihil
+                                perferendis culpa, at delectus, id est officia non?
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
+                        <div class="flex items-center">
+                            <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">
+                                Backend</div>
+                        </div>
+
+                        <div class="ml-12">
+                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, tempora aut
+                                recusandae, voluptatum sunt modi id illo dolore similique atque fuga adipisci
+                                reprehenderit quisquam quasi libero accusamus sit. Earum voluptas quasi aliquam,
+                                reprehenderit debitis, quibusdam unde ipsum dolor voluptate voluptatum nulla sint
+                                delectus quod est! Iste dolore exercitationem earum animi!
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
