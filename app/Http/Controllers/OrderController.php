1afe825ba6501467bf17b64f3376b4d9db312d5c
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-02-14 20:53:26 +0200

    Magical stock update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,6 +6,7 @@ use App\Models\Ingredient;
 use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
@@ -25,15 +26,51 @@ class OrderController extends Controller
             'qty' => 'required'
         ]);
 
-        Order::create($attributes);
+        $qty = request('qty');
+
+        $success = Order::create($attributes);
+
+        $this->deduct($success, $qty);
 
         return redirect('/');
     }
 
     public function destroy(Order $order)
     {
-        $order->delete();
+
+        $qty = $order->qty;
+
+        $success = $order->delete();
+
+        $this->increase($success, $qty);
 
         return redirect('/');
+    }
+
+    public function deduct($success, $qty)
+    {
+        if ($success) {
+
+            $deductBeef = 150 * $qty;
+            $deductCheese = 30 * $qty;
+            $deductOnion = 20 * $qty;
+
+            DB::table('ingredients')->where(['id' => 1])->decrement('qty', $deductBeef);
+            DB::table('ingredients')->where(['id' => 2])->decrement('qty', $deductCheese);
+            DB::table('ingredients')->where(['id' => 3])->decrement('qty', $deductOnion);
+        }
+    }
+    public function increase($success, $qty)
+    {
+        if ($success) {
+
+            $addBeef = 150 * $qty;
+            $addCheese = 30 * $qty;
+            $addOnion = 20 * $qty;
+
+            DB::table('ingredients')->where(['id' => 1])->increment('qty', $addBeef);
+            DB::table('ingredients')->where(['id' => 2])->increment('qty', $addCheese);
+            DB::table('ingredients')->where(['id' => 3])->increment('qty', $addOnion);
+        }
     }
 }
